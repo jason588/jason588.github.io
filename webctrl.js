845df154a4f8deb0834a2efb945750f35fee4bc8
jason588 <beyondgm5@me.com>
2018-12-05 11:48:56 +0800

    +

--- webctrl.js
+++ webctrl.js
@@ -4,13 +4,13 @@ iqiyi.style = "position: relative";
 var playDom = document.createElement("a");
 var playDomInner = document.createElement("span");
 playDomInner.innerText = "VIP点击免费观看";
-playDom.style = "color:#fff; margin:10px;position:absolute;left:0;right:0;top:0;bottom:0;background:#111;";
-playDomInner.style = "position:absolute;left:50%;right:50%;transform:translate(-50%,-50%);font-size:14px";
+playDom.style = "color:#fff; margin:10px;position:absolute;left:0;right:0;top:0;bottom:0;background:#111;z-index:999";
+playDomInner.style = "position:absolute;left:50%;top:50%;width:100%;text-align: center;transform:translate(-50%,-50%);font-size:18px";
 playDom.href = "http://baidu.com";
 playDom.appendChild(playDomInner);
 iqiyi.appendChild(playDom);
 playDom.addEventListener("click", function () {
-    alert("你好爱奇艺！！！");
+    alert("开始播放了");
     playDom.style.color = "red"
 });
 
