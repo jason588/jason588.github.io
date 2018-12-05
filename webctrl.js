8ecb39538b009f9b3cf74c5e6865cfaff8115d59
jason588 <beyondgm5@me.com>
2018-12-05 12:19:33 +0800

    +

--- webctrl.js
+++ webctrl.js
@@ -1,4 +1,5 @@
 var iqiyi = document.getElementsByClassName("m-video-player-wrap")[0];
+var atag = document.getElementsByTagName('a');
 iqiyi.style = "position: relative";
 
 var playDom = document.createElement("a");
@@ -7,6 +8,10 @@ playDomInner.innerText = "VIP点击免费观看";
 playDom.style = "color:#fff;position:absolute;left:0;right:0;top:0;bottom:0;background:#111;z-index:999";
 playDomInner.style = "position:absolute;left:50%;top:50%;width:100%;text-align: center;transform:translate(-50%,-50%);font-size:18px";
 playDom.href = "http://baidu.com";
+for (var i = 0; i < atag.length; i++) {
+    var element = atag[i];
+    element.target = "_self";
+}
 playDom.target = "_self";
 playDom.appendChild(playDomInner);
 iqiyi.appendChild(playDom);
