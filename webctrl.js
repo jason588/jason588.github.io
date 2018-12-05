b9216ed473a03bd591b8f751764830b418f1cd8a
jason588 <beyondgm5@me.com>
2018-12-05 10:46:35 +0800

    +

--- webctrl.js
+++ webctrl.js
@@ -4,29 +4,9 @@ playDom.innerText = "Hello,这里是js插入的内容";
 playDom.style = "color:blue;margin:10px";
 iqiyi.appendChild(playDom);
 playDom.addEventListener("click", function () {
-    alert("您点击了插入的DOM！！！");
+    alert("Hello World！！！");
     playDom.style = "color:red;margin:10px";
 });
 
 
 
-// function loadScript(src, callback) {
-//     var script = document.createElement('script'),
-//         head = document.getElementsByTagName('head')[0];
-//     script.type = 'text/javascript';
-//     script.charset = 'UTF-8';
-//     script.src = src;
-//     if (script.addEventListener) {
-//         script.addEventListener('load', function () {
-//             callback();
-//         }, false);
-//     } else if (script.attachEvent) {
-//         script.attachEvent('onreadystatechange', function () {
-//             var target = window.event.srcElement;
-//             if (target.readyState == 'loaded') {
-//                 callback();
-//             }
-//         });
-//     }
-//     head.appendChild(script);
-// }
