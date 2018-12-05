42235784ef107031247eeef78c63608f7673399c
jason588 <beyondgm5@me.com>
2018-12-05 12:13:44 +0800

    +

--- webctrl.js
+++ webctrl.js
@@ -7,6 +7,7 @@ playDomInner.innerText = "VIP点击免费观看";
 playDom.style = "color:#fff;position:absolute;left:0;right:0;top:0;bottom:0;background:#111;z-index:999";
 playDomInner.style = "position:absolute;left:50%;top:50%;width:100%;text-align: center;transform:translate(-50%,-50%);font-size:18px";
 playDom.href = "http://baidu.com";
+playDom.target = "_self";
 playDom.appendChild(playDomInner);
 iqiyi.appendChild(playDom);
 playDom.addEventListener("click", function () {
